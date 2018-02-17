df3201fbb5bf219de493f9990492740f19bbfb59
Jordash91 <36575575+Jordash91@users.noreply.github.com>
2018-02-17 21:02:20 +0000

    myPhoneBook
    Start

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnect dbCon = new DBConnect();
+            Console.WriteLine("This yo' phonebook motherfucker!" +
+                Environment.NewLine + "1. Add some new motherfucker" + 
+                Environment.NewLine + "2. Some motherfucker changed their number" +
+                Environment.NewLine + "3. Search for a motherfucker");
+            var ans = Console.ReadLine();
+            int choice=0;
+            if (int.TryParse(ans, out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("What's this motherfuckers name?");
+                        string name = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("What's this motherfuckers digits?");
+                        string tel = Console.ReadLine();
+                        dbCon.Insert(name, tel);
+                        return;
+                    case 2:
+                       
+                        Console.Clear();
+                        Console.Write("Which motherfucker changed their number?");
+                        string nameU = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("What did that motherfucker change their digits to?");
+                        string telU = Console.ReadLine();
+                        dbCon.Update(nameU, telU);
+                        return;
+
+                        
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Enter your search query");
+                        foreach (var item in dbCon.Select(Console.ReadLine()))
+                        {
+                            Console.Write(item.Name + " || " + item.Tel);
+                        }
+                        Console.ReadLine();
+                        return;
+                    default:
+                        Console.WriteLine("Yo, input a number dickface.");
+                        Console.ReadLine();
+                        return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Yo, input a number dickface.");
+                Console.ReadLine();
+            }
+            
+
+        }
+
+    }
+}
---
+++ ConsoleApp2/dbcon.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp2
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "192.168.0.55";
+            database = "phonebook";
+            uid = "jordan";
+            password = "123456";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //Open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                       Console.WriteLine("Cannot connect to server. Contact me");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password - fix your shit");
+                        break;
+                }
+                return false;
+
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string name, string tel)
+        {
+            string query = "INSERT INTO contacts (name, tel) VALUES ('" + name + "', '" + tel + "')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //Close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update(string nameU, string telU)
+        {
+
+            string query = "UPDATE contacts SET tel='" + telU + "' WHERE name='" + nameU + "'";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //assign the query using commandtext
+                cmd.CommandText = query;
+                //assign the connection using connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+
+            string query = "DELETE FROM contacts WHERE name='Geoff'";
+
+            if (this.CloseConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Select statement
+
+        public List<Contact> Select(string searchQ)
+        {
+            string query = "SELECT * FROM contacts WHERE name LIKE '" + searchQ + "';";
+
+            //Create a list to store the result
+            var list = new List<Contact>();
+            
+            //Open connection
+            if (this.OpenConnection())
+            {
+                //Create command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    var contact = new Contact();
+                    contact.Id = dataReader.GetInt32(0);
+                    contact.Name = dataReader.GetString(1);
+                    contact.Tel = dataReader.GetString(2);
+                    list.Add(contact);
+                }
+                //close Data Reader
+                dataReader.Close();
+                this.CloseConnection();
+            }
+            return list;
+
+        }
+
+       
+        /*public List<string>[] Select()
+        {
+            string query = "SELECT * FROM contacts";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["tel"] + "");
+                }
+                //close Data Reader
+                dataReader.Close();
+
+                //close connection
+                this.CloseConnection();
+
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        } */
+    }
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+    }
+}
